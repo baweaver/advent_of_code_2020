d134034e5a9157509d603be25574c6e432760b54
Brandon Weaver <keystonelemur@gmail.com>
2020-12-28 20:20:20 -0600

    fix 1b

--- 01_b_report_repair.rb
+++ 01_b_report_repair.rb
@@ -3,11 +3,9 @@ File.readlines(ARGV[0]).then do |input|
   puts input
     # First we need integers to work with
     .map(&:to_i)
-    # We can make this into an Enumerator to make sure we don't
-    # iterate every possible combination, just however many we
-    # need to find what we want to find
-    .to_enum(:combination, 3)
-    # So we can literally find it by summing up the combinations,
+    # Then we get all combinations of 3
+    .combination(3)
+    # So we can find it by summing up the combinations,
     # a three item array, and see if it gets to 2020.
     #
     # Do note that you can only use numbered params one level deep, hence
